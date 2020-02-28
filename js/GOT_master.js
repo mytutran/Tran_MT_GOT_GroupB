a13e5c8a83ba831747ee583e7b99586e48dcfb45
Tran <m_tran110573@ats-hr420-dm16.fc.ca>
2020-02-28 14:38:10 -0500

    done

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -3,6 +3,9 @@
   let sigilButtons = document.querySelectorAll(".sigilContainer"),
       lightBox = document.querySelector(".lightbox"),
       houseVideo = lightBox.querySelector("video"),
+      pauseButton = lightBox.querySelectorAll(".pause-button"),
+      rewindButton = lightBox.querySelectorAll(".rewind-button"),
+      playButton = lightBox.querySelectorAll(".play-button"),
       closeButton = lightBox.querySelector(".close-button");
 
   //functions go in the middle -> what do we want our app to do?
@@ -22,10 +25,25 @@
     houseVideo.currentTime = 0;
   }
 
+  function rewindVideo () {
+    houseVideo.currentTime = 0;
+  }
+
+  function pauseVideo () {
+    houseVideo.pause();
+  }
+
+  function playVideo () {
+    houseVideo.play();
+  }
+
+
   // event handling for our sigilButtons
   sigilButtons.forEach(button => button.addEventListener("click", showLightBox));
 
   // add some event handling for the lightbox close button
   closeButton.addEventListener("click", hideLightBox);
-
+  pauseButton.forEach(button => button.addEventListener("click", pauseVideo));
+  rewindButton.forEach(button => button.addEventListener("click", rewindVideo));
+  playButton.forEach(button => button.addEventListener("click", playVideo));
 })();
